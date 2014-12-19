2e694f22da455b735676d261143078d79f7ab70f
bmcorser <bmcorser@gmail.com>
2014-12-19 22:16:33 +0000

    less fun

--- glp.py
+++ glp.py
@@ -17,7 +17,7 @@ def node(prefix, colour, git_id):
                       color=colour)
 
 commit_node = functools.partial(node, 'commit', '#FF851B')
-name_node = functools.partial(node, 'name', '#2ECC40')
+file_node = functools.partial(node, 'file', '#2ECC40')
 tree_node = functools.partial(node, 'tree', '#FFDC00')
 blob_node = functools.partial(node, 'blob', '#7FDBFF')
 
@@ -28,14 +28,13 @@ def graph_tree_entries(repo, graph, parent, entry):
     git_object = repo.get(git_id)
     if isinstance(git_object, pygit2.Blob):
         content = blob_node(git_id)
-        name = name_node(entry.name)
+        name = file_node(entry.name)
         graph.add_node(content)
         graph.add_node(name)
         graph.add_edge(pydot.Edge(parent, name, dir='back'))
         graph.add_edge(pydot.Edge(name, content, dir='back'))
         graph.add_node(blob_node(git_id))
     if isinstance(git_object, pygit2.Tree):
-        print('tree', parent.to_string())
         node = tree_node(entry.name)
         graph.add_node(node)
         graph.add_edge(pydot.Edge(parent, node, dir='back'))
@@ -47,13 +46,12 @@ def graph_tree_entries(repo, graph, parent, entry):
 @click.argument('ref', default='HEAD')
 def main(repo_path, ref):
     repo = pygit2.Repository(repo_path)
+    tree = repo.revparse_single(ref).tree
     graph = pydot.Dot(ref, graph_type='digraph')
-    for commit in repo.walk(repo.revparse_single(ref).id,
-                            pygit2.GIT_SORT_TIME):
-        root_node = commit_node(commit.id.hex[:7])
-        graph.add_node(root_node)
-        for entry in commit.tree:
-            graph_tree_entries(repo, graph, root_node, entry)
+    root_node = commit_node(ref)
+    graph.add_node(root_node)
+    for entry in tree:
+        graph_tree_entries(repo, graph, root_node, entry)
     graph.write_svg("{0}.svg".format(ref))
 
 if __name__ == '__main__':
